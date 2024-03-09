2c514c7094708cef813a4bf410cdd03992c7f88b
aniskehila <aniskehila22@gmail.com>
2024-03-10 00:14:23 +0100

    add count route to count packages states

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -4,8 +4,26 @@ import { auth } from "../utils/helpers";
 
 const packageRouter = Router();
 
-packageRouter.get("/", auth, (req, res) => {
-  console.log(req.body);
+packageRouter.get("/count", auth, async (req, res) => {
+  const { id } = req.body.decoded;
+  const data = await db.package.findMany({
+    where: {
+      userId: id,
+    },
+    select: {
+      status: true,
+    },
+  });
+  if (!data)
+    return res.status(401).json({ message: "This account has no data" });
+  const count = {
+    total: data.length,
+    delivered: data.reduce((i, e) => (e.status === "DELIVERED" ? i + 1 : i), 0),
+    confirmed: data.reduce((i, e) => (e.status === "CONFIRMED" ? i + 1 : i), 0),
+    onRoad: data.reduce((i, e) => (e.status === "ON_ROAD" ? i + 1 : i), 0),
+    returned: data.reduce((i, e) => (e.status === "RETURNED" ? i + 1 : i), 0),
+  };
+  res.json({ count });
 });
 
 export default packageRouter;
